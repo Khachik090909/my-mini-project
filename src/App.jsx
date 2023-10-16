62a2bcc6a313589ca7a28e8436806a547254f33c
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-16 17:15:00 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useState, useEffect } from "react";
 import DataContacts from "./components/Data";
 import "./App.css";
 import ContactList from "./components/ContactList";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   //state (état, données)
   const [dataContacts, setDataContacts] = useState(DataContacts);
   console.log(dataContacts);
+
   //comportements (dynamique)
   const handleRemoveClick = (id) => {
     const removeContact = dataContacts.filter((contact) => contact.id !== id);
