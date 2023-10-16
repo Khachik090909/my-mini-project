d0e2e43a50c26f94f69af0d0c677cf922ad1adec
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-16 14:06:40 +0200

    create ContactList and add removeContact function

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,22 @@
+function ContactList({ dataContacts, handleRemoveClick }) {
+  //state (état, données)
+
+  //comportements (dynamique)
+
+  //affichage (render)
+  return (
+    <div className="mainContainer">
+      <h1>Liste de Contatcs</h1>
+      {dataContacts.map((elem) => (
+        <li className="contactList" key={elem.id}>
+          <img src={elem.img} alt={elem.prenom} />
+          <p>{elem.nom}</p>
+          <p>{elem.prenom}</p>
+          <button onClick={() => handleRemoveClick(elem.id)}>X</button>
+        </li>
+      ))}
+    </div>
+  );
+}
+
+export default ContactList;
