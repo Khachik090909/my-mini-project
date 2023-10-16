9248dbf0841eb9366af55d648616ee672108c899
Khachik090909 <khachiksahakyan90@gmail.com>
2023-10-16 10:31:26 +0200

    add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import React from 'react'
+import DataContacts from './components/Data'
 import './App.css'
 
 function App() {
 
 
   return (
-  
+    
+  <div></div>
   )
 }
 
---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+const DataContacts = [
+  {
+    nom: "Homer",
+    prenom: "Simpson",
+    tel: "06 12 34 56 78",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
+  },
+  {
+    nom: "Marge",
+    prenom: "Simpson",
+    tel: "06 12 34 56 66",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png",
+  },
+];
