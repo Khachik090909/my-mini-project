d7541a286ed307c7c749c8b66ed81b9352bf79ba
DELMAS <delmastristan64@gmail.com>
2023-10-17 11:25:04 +0200

    add style to form

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -6,7 +6,7 @@ function ContactList({ data, handleRemoveClick }) {
   //affichage (render)
   return (
     <div className="mainContainer">
-      <h1>Liste de Contacts</h1>
+      <h1>Mes Contacts</h1>
       {data.map((elem) => (
         <li className="contactList" key={elem.id}>
           <img src={elem.img} alt={elem.prenom} />
